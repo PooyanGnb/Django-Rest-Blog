c6ad92feca2484899931513a1a72acd41507b598
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-06-12 16:36:45 +0330

    [ADD] RedirectView & ListView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import indexView, IndexView
+from . import views
 
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('', indexView, name="test"),
-    path("cbv/", IndexView.as_view(), name="cbv-index"),
+    path("cbv/", views.IndexView.as_view(), name="cbv-index"),
+    path("post/", views.PostList.as_view(), name="post-list"),
+    path("linkedin/", views.RedirectToLinkedIn.as_view(), name="linkedin-redirect"),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,12 +1,13 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
-def indexView(request):
-    return render(request, 'index.html')
-
 class IndexView(TemplateView):
+    # a class based view to show index page
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
@@ -14,3 +15,19 @@ class IndexView(TemplateView):
         context['name'] = 'ali'
         return context
 
+
+class RedirectToLinkedIn(RedirectView):
+    # a class based view to redirect to LinkedIn
+    url = 'https://www.linkedin.com/in/pooyan-ghanbari/'
+
+
+class PostList(ListView):
+    # a class based view to show post list
+    # model = Post
+    context_object_name = 'posts'
+    paginate_by = 2
+    ordering = 'id'
+
+    def get_queryset(self):
+        posts = Post.objects.filter(status=True).order_by(self.ordering)
+        return posts
