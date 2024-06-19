7f8606137a20a1d915d74834687a404800c13c8e
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-06-19 13:21:41 +0330

    [UPDATE] api view

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,14 +2,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view()
 def postList(request):
-    return Response('ok')
+    posts = Post.objects.filter(status=True)
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def postDetail(request, id):
-    post = Post.objects.get(pk=id)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+        post = get_object_or_404(Post, pk=id, status=True)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    # try:
+    # except Post.DoesNotExist:  # creating a 404 response / or do it with get_object_or_404 instead of get
+    #     return Response({'detail':'Post does not exist'}, status=status.HTTP_404_NOT_FOUND)
