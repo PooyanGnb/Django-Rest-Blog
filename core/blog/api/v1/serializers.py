a836d779097ad184f7ff05c47d255327a3398491
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-07-03 10:59:11 +0330

    [update] serializer category in post

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,9 +10,15 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+    category = serializers.SlugRelatedField(many=False, slug_field='name', queryset=Category.objects.all())
     class Meta:
         model = Post
-        fields = ["id", "author", "title", "content", "snippet", "status", "relative_url", "created_date", "published_date"]
+        fields = ["id", "author", "title", "content", "category", "snippet", "status", "relative_url", "absolute_url", "created_date", "published_date"]
+    
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
 
 
 class CategorySerializer(serializers.ModelSerializer):
