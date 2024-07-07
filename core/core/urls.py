08817c44e5c44220f0b25179b41072511a35f101
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-07-07 15:30:43 +0330

    [ADD] account api v1

--- core/core/urls.py
+++ core/core/urls.py
@@ -26,9 +26,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Blog api",
       default_version='v1',
-      description="Test description",
+      description="a test document for the blog api",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -39,11 +39,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api-docs/', include_docs_urls(title='api_sample')),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/o.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
