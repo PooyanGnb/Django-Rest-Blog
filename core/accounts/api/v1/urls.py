9c056a865c196ff0cec5c5454732823bfb0b30e3
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-07-13 10:36:40 +0330

    [ADD] logout discard token

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -7,9 +7,10 @@ app_name = 'api-v1'
 urlpatterns = [
     # registration
     path('registration', views.RegistrationApiView.as_view(), name='registration'),
-    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token login'), 
     # change password
     # reset password
     # login token 
+    path('token/login/', views.CustomObtainAuthToken.as_view(), name='token login'), 
+    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token logout'), 
     # login jwt
 ]
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -4,6 +4,8 @@ from .serializers import RegistrationSerializer, CustomAuthTokenSerializer
 from rest_framework import status 
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 
 class RegistrationApiView(generics.GenericAPIView):
@@ -35,3 +37,11 @@ class CustomObtainAuthToken(ObtainAuthToken):
             'user_id': user.pk,
             'email': user.email
         })
+    
+
+class CustomDiscardAuthToken(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post (self, request, *args, **kwargs):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
