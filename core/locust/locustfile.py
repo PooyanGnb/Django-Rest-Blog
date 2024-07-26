06e6f632afd018871672de8501bc0b61a196620d
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-07-26 21:54:09 +0330

    [add] locust with login

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -3,6 +3,17 @@ from locust import HttpUser, task
 
 class QuickstartUser(HttpUser):
 
+    def on_start(self):
+        response = self.client.post('/accounts/api/v1/jwt/create/', data={
+            "email": "admin@admin.com",
+            "password": "123"
+        }).json()
+        self.client.headers.update({"Authorization": f"Bearer {response.get('access', '')}"})
+
     @task
     def post_list(self):
         self.client.get('/blog/api/v1/post/')
+
+    @task
+    def category_list(self):
+        self.client.get('/blog/api/v1/category/')
