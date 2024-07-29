62f6eea184d4b5e0c6f5f3a49315133f2e858ae6
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-07-29 16:01:16 +0330

    [add] stage docker and caching and gunicorn

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,17 +1,24 @@
 from django.http import HttpResponse, JsonResponse
 from django.core.cache import cache
+from django.views.decorators.cache import cache_page
 from .tasks import sendEmail
 import requests
 
 
+@cache_page(60)
 def test(request):
-    # checks if the data is available in the cache
-    if cache.get('test_delay_api') is None:
-        # if it's not available, then make a request to fetch the data
-        response = requests.get()
-        # store the response in the cache and set the timeout to 60s
-        cache.set('test_delay_api', response.json(), 60)
-    return JsonResponse(cache.get('test_delay_api'))
+    response = requests.get()
+    return JsonResponse(response.json())
+
+
+# def test(request):
+#     # checks if the data is available in the cache
+#     if cache.get('test_delay_api') is None:
+#         # if it's not available, then make a request to fetch the data
+#         response = requests.get()
+#         # store the response in the cache and set the timeout to 60s
+#         cache.set('test_delay_api', response.json(), 60)
+#     return JsonResponse(cache.get('test_delay_api'))
 
 
 def send_email(request):
